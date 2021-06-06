c3f49be6920b5e434e7c0c3023be9194eb34e8c1
tsnazar <tsnazr3@gmail.com>
2021-06-06 18:10:25 +0300

    fixes

--- app.js
+++ app.js
@@ -28,9 +28,23 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+if (app.get('env') == 'development') {
+  var sassMiddleware = require('node-sass-middleware');
+  app.use(logger('dev'));
+  app.use(express.json({limit: '50mb'}));
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+  app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+  }));
+} else {
+  app.use(logger('combined'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+}
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
